e3cb66506eb700079bd1e19aa4eaefb0610cc695
BojeUbeyteMenya <mralexgold01@gmail.com>
2023-05-10 23:11:08 +1000

    привет мир

---
+++ ListOfBoards/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfBoards
+{
+    class List
+    {
+        class Board
+        {
+            string data;
+            string creater;
+            string task;
+        }
+    }
+}
